962e184b734b9666a1139819d509900561cbb59f
jamesarrojo.01 <jamesarrojo.01@gmail.com>
2022-09-08 08:18:01 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import Artists from './components/Artists'
+import Building from './components/Building'
+import CallToAction from './components/CallToAction'
+import Companies from './components/Companies'
+import Navbar from './components/Navbar'
+import Recommended from './components/Recommended'
+import Tickets from './components/Tickets'
+import Welcome from './components/Welcome'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Welcome />
+      <Companies />
+      <Tickets />
+      <Artists />
+      <Recommended />
+      <Building />
+      <CallToAction />
+      <Footer />
+    </>
+    
+  )
+}
+
+export default App
